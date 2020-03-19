74922ed67bf57b4dc450acfe01f5fdeecb0ce203
JingtianJia <jiajingtian@qq.com>
2020-03-19 19:54:21 +0800

    删除多余注解

--- repast-app-consumer-management/repast-app-memberinfo-consumer-6081/src/main/java/com/aaa/lee/repast/controller/PmsCommentController.java
+++ repast-app-consumer-management/repast-app-memberinfo-consumer-6081/src/main/java/com/aaa/lee/repast/controller/PmsCommentController.java
@@ -1,7 +1,5 @@
 package com.aaa.lee.repast.controller;
 
-import com.aaa.lee.repast.annotation.LoginLogAnnotation;
-import com.aaa.lee.repast.annotation.TokenAnnotation;
 import com.aaa.lee.repast.base.BaseController;
 import com.aaa.lee.repast.base.ResultData;
 import com.aaa.lee.repast.model.PmsComment;
@@ -19,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class PmsCommentController  extends BaseController {
     @Autowired
     private IRepastService iRepastService;
-    @TokenAnnotation
     @GetMapping("/byIdSelectComment")
     @ApiOperation(value = "根据商品查评论", notes = "查询此商品所有评论(token，shopId)")
     ResultData byIdSelectComment(@RequestParam("token") String token, @RequestParam("shopId") int shopId){
@@ -27,7 +24,6 @@ public class PmsCommentController  extends BaseController {
             return iRepastService.byIdSelectComment(token, shopId);
         }
 
-        @TokenAnnotation
         @ApiOperation(value = "根据订单id查评论", notes = "查询此商品所有评论(token，orderid)")
         @GetMapping("/byOrderIdSelectComment")
         ResultData byOrderIdSelectComment(@RequestParam("token") String token,@RequestParam("orderId") int orderId){
@@ -35,7 +31,6 @@ public class PmsCommentController  extends BaseController {
         }
 
 
-        @TokenAnnotation
         @PostMapping("/insertComment")
         @ApiOperation(value = "新增评论", notes = "新增商品评论(token，pmsComment（实体类中时间不用）)")
         ResultData insertComment(@RequestParam("token") String token,PmsComment pmsComment){
@@ -47,7 +42,6 @@ public class PmsCommentController  extends BaseController {
 
 
 
-        @TokenAnnotation
         @ApiOperation(value = "根据订单id删除评论", notes = "根据订单id删除评论(token，pmsComment)")
         @PostMapping("/deleteComment")
         ResultData deleteComment(@RequestParam("token") String token, PmsComment pmsComment){

56798ea2754abdf41b960f7faaab03b0c46fec33
JingtianJia <jiajingtian@qq.com>
2020-03-21 21:31:20 +0800

    增加根据product id回滚stock方法

--- repast-app-management/repast-app-management-mapper/src/main/java/com/aaa/lee/repast/mapper/ProductMapper.java
+++ repast-app-management/repast-app-management-mapper/src/main/java/com/aaa/lee/repast/mapper/ProductMapper.java
@@ -1,6 +1,7 @@
 package com.aaa.lee.repast.mapper;
 
 import com.aaa.lee.repast.model.Product;
+import org.apache.ibatis.annotations.Param;
 import tk.mybatis.mapper.common.Mapper;
 
 /**
@@ -11,4 +12,6 @@ public interface ProductMapper extends Mapper<Product> {
     Product selectProductGiftPoint(int id);
     //修改商品的库存
     Integer updateProductStock(Product product);
+    //根据商品id和数量回滚库存
+    Integer rollbackStock(@Param("id") Long id, @Param("num") Integer num);
 }

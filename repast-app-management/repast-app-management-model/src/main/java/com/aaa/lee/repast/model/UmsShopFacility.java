c611c916ebfcf9fc81f1799bbb17f27b21243d36
JingtianJia <jiajingtian@qq.com>
2020-03-22 17:53:48 +0800

    规范类

--- repast-app-management/repast-app-management-model/src/main/java/com/aaa/lee/repast/model/UmsShopFacility.java
+++ repast-app-management/repast-app-management-model/src/main/java/com/aaa/lee/repast/model/UmsShopFacility.java
@@ -4,6 +4,7 @@ import lombok.*;
 import lombok.experimental.Accessors;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Table(name = "ums_shop_facility")
 @Data
@@ -12,7 +13,7 @@ import javax.persistence.*;
 @Accessors(chain = true)
 @ToString
 @EqualsAndHashCode
-public class UmsShopFacility {
+public class UmsShopFacility implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -25,74 +26,4 @@ public class UmsShopFacility {
     private String description;
 
     private String icon;
-
-    /**
-     * @return id
-     */
-    public Long getId() {
-        return id;
-    }
-
-    /**
-     * @param id
-     */
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    /**
-     * @return shop_id
-     */
-    public Long getShopId() {
-        return shopId;
-    }
-
-    /**
-     * @param shopId
-     */
-    public void setShopId(Long shopId) {
-        this.shopId = shopId;
-    }
-
-    /**
-     * @return title
-     */
-    public String getTitle() {
-        return title;
-    }
-
-    /**
-     * @param title
-     */
-    public void setTitle(String title) {
-        this.title = title == null ? null : title.trim();
-    }
-
-    /**
-     * @return description
-     */
-    public String getDescription() {
-        return description;
-    }
-
-    /**
-     * @param description
-     */
-    public void setDescription(String description) {
-        this.description = description == null ? null : description.trim();
-    }
-
-    /**
-     * @return icon
-     */
-    public String getIcon() {
-        return icon;
-    }
-
-    /**
-     * @param icon
-     */
-    public void setIcon(String icon) {
-        this.icon = icon == null ? null : icon.trim();
-    }
 }
